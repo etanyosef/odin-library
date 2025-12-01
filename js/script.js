7f0df2f8fd8e6046d3371bdc130e7dd4870a84a3
Ethan <ethancredo.ascc@outlook.com>
2025-12-01 15:17:10 +0800

    put form inside dialog tag and and button to show and close it

--- js/script.js
+++ js/script.js
@@ -5,22 +5,81 @@ function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.info = function() {
-        return `Id: ${this.id}, Title: ${this.title}, Title: ${this.author}, Pages: ${this.pages}`;
-    }
+    // this.info = function() {
+    //     return `Id: ${this.id}, Title: ${this.title}, Title: ${this.author}, Pages: ${this.pages}`;
+    // }
 }
 
+// add default books manually
+const book1 = new Book('Miming', 'Sagit', '500');
+myLibrary.push(book1);
+const book2 = new Book('Dogoy', 'Sevee', '400');
+myLibrary.push(book2);
+
+
 function addBookToLibrary(e) {
-    const title = document.getElementById('title').value;
-    const author = document.getElementById('author').value;
-    const pages = document.getElementById('pages').value;
+    const bookTitle = document.getElementById('title');
+    const bookAuthor = document.getElementById('author');
+    const bookPages = document.getElementById('pages');
+
+    const title = bookTitle.value;
+    const author = bookAuthor.value;
+    const pages = bookPages.value;
 
     const book = new Book(title, author, pages);
-
+    // add new book to myLibrary[]
     myLibrary.push(book);
-    console.log(myLibrary);
+
+    // clear inputs
+    bookTitle.value = '';
+    bookAuthor.value = '';
+    bookPages.value = '';
+
+    console.table(myLibrary);
+
+    // close form dialog
+    dialog.close();
+
     e.preventDefault();
 }
 
 const btnAddBook = document.getElementById('add-book');
 btnAddBook.addEventListener('click', addBookToLibrary);
+
+const tableBooks = document.getElementById('table-books');
+
+// display books inside a table
+for (let key in myLibrary)  {
+    if (myLibrary.hasOwnProperty(key)) {
+        const book = myLibrary[key];
+
+        const rowBook = document.createElement('tr');
+        // loop through book and get all values inside
+        Object.keys(book).forEach(key => {
+            // get each value inside book object
+            let value = book[key];
+
+            const td = document.createElement('td');
+            td.textContent = value;
+            rowBook.append(td);
+        });
+
+        tableBooks.append(rowBook);
+
+    }
+}
+
+const dialog = document.querySelector('dialog');
+const showFormButton = document.getElementById('show-book-form');
+const closeFormButton = document.getElementById('close-form');
+
+// close dialog
+closeFormButton.addEventListener('click', (e) => {
+    dialog.close();
+    e.preventDefault();
+});
+
+// show add book form dialog
+showFormButton.addEventListener('click', () => {
+    dialog.showModal();
+});
