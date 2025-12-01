0825c8ab38665d5a6ef491c7dd1d9ed2b745753d
Ethan <ethancredo.ascc@outlook.com>
2025-12-01 15:52:42 +0800

    add isRead in book and UI

--- js/script.js
+++ js/script.js
@@ -1,19 +1,20 @@
 const myLibrary = [];
 
-function Book(title, author, pages) {
+function Book(title, author, pages, isRead) {
     this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.isRead = isRead;
     // this.info = function() {
     //     return `Id: ${this.id}, Title: ${this.title}, Title: ${this.author}, Pages: ${this.pages}`;
     // }
 }
 
 // add default books manually
-const book1 = new Book('Miming', 'Sagit', '500');
+const book1 = new Book('Miming', 'Sagit', '500', false);
 myLibrary.push(book1);
-const book2 = new Book('Dogoy', 'Sevee', '400');
+const book2 = new Book('Dogoy', 'Sevee', '400', true);
 myLibrary.push(book2);
 
 
@@ -21,12 +22,13 @@ function addBookToLibrary(e) {
     const bookTitle = document.getElementById('title');
     const bookAuthor = document.getElementById('author');
     const bookPages = document.getElementById('pages');
+    const bookIsRead = document.getElementById('is-read');
 
     const title = bookTitle.value;
     const author = bookAuthor.value;
     const pages = bookPages.value;
 
-    const book = new Book(title, author, pages);
+    const book = new Book(title, author, pages, isRead);
     // add new book to myLibrary[]
     myLibrary.push(book);
 
@@ -68,6 +70,7 @@ function displayBook(book) {
         let value = book[key];
         const td = document.createElement('td');
         td.textContent = value;
+        rowBook.setAttribute('data-index-number', book.id);
         rowBook.append(td);
     });
     tableBooks.append(rowBook);
